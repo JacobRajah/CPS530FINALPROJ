1f2900c48ad847edfd536414810c26ffdbea22eb
JacobRajah <jacob.rajah@mail.utoronto.ca>
2021-11-08 19:07:13 -0500

    Add details to page3 and css file

--- client/src/components/page3/page3.js
+++ client/src/components/page3/page3.js
@@ -1,4 +1,5 @@
 import Nav from "../navigation/navbar";
+import './page3.css';
 
 function Page3()
 {
@@ -7,6 +8,7 @@ function Page3()
             <Nav></Nav>
             <div>
                 <h1>This is Page 3</h1>
+                <p>Jacob is working on this page</p>
             </div>
         </>
     );
