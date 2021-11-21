3a4110927357d2402a4969d35df4479523553589
JacobRajah <jacob.rajah@mail.utoronto.ca>
2021-11-20 23:46:42 -0500

    Server: Heroku Init

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const exprees = require('express');
+const express = require('express');
+const path = require('path');
 
-const app = exprees();
+const app = express();
+
+// Serve static files from the react app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 const image_reel = [
     {id: 0, url: 'https://cdn.escapism.to/featured_image/5fbd1964287f4.jpeg'},
@@ -28,7 +32,11 @@ app.get('/api/img', (req, res) => {
     curr_img_id += 1;
 });
 
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
-const port = 5000;
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server started on port ${port}`));
