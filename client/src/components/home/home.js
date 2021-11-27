2a64a3b606fbdbddd33e0a9d658c04b422447f06
XtremeNV101 <avarghese@ryerson.ca>
2021-11-27 09:53:21 -0500

    FIxed homepage and stated page 5

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -9,21 +9,21 @@ function Home()
 {    
     return(
         <div className="homepage">
-            <Nav/>
+            <Nav></Nav>
             <h1 className="h1-p1">Homepage</h1>
-            <h2 className="title">FrontEnd Language: React.js</h2>
+            <h2 className="h2-p1">FrontEnd Language: React.js</h2>
             <figure>
             <img className="img-p1" src={React} alt="React Symbol"></img>
             <figcaption className="figcaption-p1"> Figure 1. React.js</figcaption>
             </figure>
-            <h3 className="title">Popularity</h3>
-            <p className="title">React is a JavaScript library that specializes in helping developers build user interfaces, or UIs.
+            <h3 className="h3-p1">Popularity</h3>
+            <p className="para">React is a JavaScript library that specializes in helping developers build user interfaces, or UIs.
             Due to the language be declarative, which means to expresses the logic of a computation without describing its control flow.
             With Reach, HTML is written using JavaScript, meaning the code that is written depends on the data. What makes React popular is that
             it designs simple views for each state in your application, and the library will efficiently update and render just the right component when your data changes.
             Due to this simplicity and flexibility, many people refer to React as the future of web develompent.</p>
-            <h3 className="title">Strengths</h3>
-            <ul className="title">
+            <h3 className="h3-p1">Strengths</h3>
+            <ul className="para">
             <li>Makes JavaScript coding easier.</li>
             <li>Extremely competent.</li>
             <li>Excellent cross-platform support.</li>
@@ -33,33 +33,33 @@ function Home()
             <li>UI focused designs.</li>
             <li>Easy to adopt</li>
             </ul>
-            <h3 className="title">Weaknesses</h3>
-            <ul className="title">
+            <h3 className="h3-p1">Weaknesses</h3>
+            <ul className="para">
             <li>ReactJS components are difficult to reuse in complex interactive web projects.</li>
             <li>React technologies updating and accelerating so fast that there is no time to make proper documentation</li>
             <li>ReactJS Covers only the UI Layers of the app and nothing else. </li>
             </ul>
 
-            <h2 className="title">Backend Language: Express.js</h2>
+            <h2 className="h2-p1">Backend Language: Express.js</h2>
             <figure>
-            <img src={Express} alt="Express Symbol"></img>
-            <figcaption> Figure 2. Express.js</figcaption>
+            <img className="img-p1" src={Express} alt="Express Symbol"></img>
+            <figcaption className="figcaption-p1"> Figure 2. Express.js</figcaption>
             </figure>
-            <h3 className="title">Popularity</h3>
-            <p className="title">Express js is a backend development framework for Node js. The Language is
+            <h3 className="h3-p1">Popularity</h3>
+            <p className="para">Express js is a backend development framework for Node js. The Language is
             designed to build single-page, multi-page, and hybrid web applications. It is the de facto standard server framework for node. Because it is 
             built on Node.js, it has more features and, therefore, can be used to develop web applications. Because of its versatility and accessibility,
             it is popular to use as a a backend language, especially when it is easy to configure and customize </p>
-            <h3 className="title">Strengths</h3>
-            <ul className="title">
+            <h3 className="h3-p1">Strengths</h3>
+            <ul className="para">
             <li>Built to ensure repetition of code does not occur; less coding time</li>
             <li>Routing and controller is provided</li>
             <li>It can handle many simultaneous requests without straining the server.</li>
             <li>Includes various middleware modules which you can use to perform additional tasks on request and response.</li>
             <li>Easy to connect with databases such as MongoDB, Redis, MySQL</li>
             </ul>
-            <h3 className="title">Weaknesses</h3>
-            <ul className="title">
+            <h3 className="h3-p1">Weaknesses</h3>
+            <ul className="para">
             <li>It's not opinionated in how it preferred to be implemented in your app and won't hold your hand in showing you how to structure an efficient Express app.</li>
             <li>It's not big on security; Express doesn't dictate how security is implemented either - that's entirely up to your app.</li>
             <li>Express js incorporate code patterns to avoid repetition. This can cause a problem in debugging if script is incorrect </li>
--- client/src/components/page5/page5.js
+++ client/src/components/page5/page5.js
@@ -1,14 +1,39 @@
+import './page5.css'
 import Nav from "../navigation/navbar";
-
+import Front from "../../images/Front_end.jpg";
+import Back from "../../images/Back_end.jpg";
 function Page5()
 {
     return (
-        <>
-            <Nav></Nav>
-            <div>
-                <h1>This is Page 5</h1>
+           
+            <div className="page5">
+                 <Nav></Nav>
+                <h1 className = "main-title">Conclusion</h1>
+                <h2 className="h2-p1">React.js Experience</h2>
+                <figure>
+                <img className="img-p3" src={Front} alt="Front End"></img>
+                </figure>
+                <p className="para">Overall React was very easy to learn, especially for people who had experience in in programming.
+                For us, it was easier to code in React than in jQuery due to the syntax being similar to previous languages. However, this is considering
+                that we already have coded in HTML, CSS, and JavaScript before. The difficult part of Reaxt was the installation; however, websites for this process
+                helped navigate through the installation. Due to this, it is easier to say that we would have not done anything different when using React and we 
+                are glad we chose it. In conclusion, this is our thought of react: You can take React and easily plug it into whatever you have created and use it as you see fit. 
+                Again, to do this well requires knowledge of application architecture. 
+                This is especially important on mobile devices, where the power and thermal-constrained environment can wreak havoc on a 
+                badly-architected application.</p>
+                <h2 className="h2-p1">Express.js Experience</h2>
+                <figure>
+                <img className="img-p3" src={Back} alt="Back End"></img>
+                </figure>
+                <p className="para">Overall React was very easy to learn, especially for people who had experience in in programming.
+                For us, it was easier to code in React than in PHP due to the syntax being similar to previous languages. However, this is considering
+                that we already have coded in HTML, CSS, and JavaScript before. The difficult part of Reaxt was the installation; however, websites for this process
+                helped navigate through the installation. Due to this, it is easier to say that we would have not done anything different when using React and we 
+                are glad we chose it. In conclusion, this is our thought of react: You can take React and easily plug it into whatever you have created and use it as you see fit. 
+                Again, to do this well requires knowledge of application architecture. 
+                This is especially important on mobile devices, where the power and thermal-constrained environment can wreak havoc on a 
+                badly-architected application.</p>
             </div>
-        </>
     );
 }
 
